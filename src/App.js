a0b323b086ae58d505c7698322abeb0b6ff33819
Baxtiyor <jamine.anouche.3y@lovebite.net>
2021-06-09 12:08:19 +0500

    portfolio qismi

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import Homepage from './components/Homepage';
 import About from "./components/About";
 import './components/styles/app.scss'
 import Navbars from "./components/Navbars";
+import Portfolio from "./components/Portfolio"
 
 
 function App() {
   return (
     <div>
-    <Navbars/>
-      <BrowserRouter>
 
+      <BrowserRouter>
+      <Navbars/>
       <Switch>
 
       <Route path='/' exact component={Homepage}/>
       <Route path="/about" exact component={About}/>
+      <Route path="/portfolio" exact component={Portfolio}></Route>
 
 
       </Switch>
